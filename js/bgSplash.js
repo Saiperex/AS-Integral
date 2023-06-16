d6b3a4826bfb38165892359eb3a3c5a921b97b41
Xavier <xaviramos668@gmail.com>
2023-06-15 21:30:06 -0300

    Mejorando el SplashScreen

--- js/bgSplash.js
+++ js/bgSplash.js
@@ -1,20 +1,19 @@
 const splash = document.querySelector(".splashPane");
-const imagenesS = ["splash1.jpeg", "splash2.jpeg", "splash3.jpeg", "splash4.jpeg"];
-var indexS = 1;
-
-function cambiarBackground() 
-{
-    splash.style.backgroundImage = `url('recursos/splash/${imagenesS[indexS]}')`;
-    indexS = indexS + 1
-    if (indexS > 3) {
-        indexS = 0
+function cambiarContenido(opcion) {
+    var contenidoGrande = document.getElementById("contenido-grande");
+    
+    // Lógica para cambiar el contenido según la opción seleccionada
+    if (opcion === 1) {
+      contenidoGrande.innerHTML = "<img src='recursos/splash/splash2.jpeg' alt='Imagen 1'>";
+    } else if (opcion === 2) {
+      contenidoGrande.innerHTML = "<video src='recursos/splash/splash1.mp4' autoplay muted controls></video>";
+    } else if (opcion === 3) {
+      contenidoGrande.innerHTML = "<img src='recursos/splash/splash4.jpeg' alt='Imagen 2'>";
+    } else if (opcion === 4) {
+      contenidoGrande.innerHTML = "<video src='recursos/splash/splash3.mp4' autoplay muted controls></video>";
     }
-    console.log(imagenesS[indexS])
-
-}
-
-setInterval(cambiarBackground, 5000);
-document.querySelector(".fa-square-xmark").addEventListener("click",function()
-{
-    splash.style.display="none"
-})
+  }
+  const cerrarButton=document.querySelector(".fa-square-xmark")
+  cerrarButton.addEventListener("click",function(){
+    document.querySelector(".splashPane").style="display:none"
+  })
